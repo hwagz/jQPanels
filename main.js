9d4edf38b52eb4fc73ea8eb602d6f923f1b2275e
hwagz <hunt.wagner@gmail.com>
2016-04-19 13:04:51 -0400

    Style changes

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 $(function(){
 
+  var colors = ["red","blue","fuchsia"];
+  var grey = "#666";
+  var randomColor = function(){
+      return colors[Math.floor(Math.random()*colors.length)];
+    };
+  var $active = $('.active');
+
   $('.tab-panels .tabs li').on('click',function(){
     var slideTime = 200;
+    var newColor = randomColor();
     var $panel = $(this).closest('.tab-panels');
-    $panel.find('.tabs li.active').removeClass('active');
-    $(this).addClass('active');
+    $active.css({backgroundColor: grey});
+    $panel.find('.tabs li.active').removeClass('active').css({backgroundColor: grey});
+    $(this).addClass('active').css({backgroundColor: newColor});
+
     //figure out which panel to show
     var panelToShow = $(this).attr('rel');
 
     //hide current panel
+
     $panel.find('.panel.active').slideUp(slideTime, showNextPanel);
 
     function showNextPanel(){
-      $(this).removeClass('active');
+
+      $(this).removeClass('active').css({backgroundColor: grey});
       $('#'+panelToShow).slideDown(slideTime, function(){
-        $(this).addClass('active');
+        $(this).addClass('active').css({backgroundColor: newColor});
       })
     }
 
 
 
-
+    return false;
     //show new panel
   });
 
